3cbc3a93de78555289b06f959ca21b61ce870db7
zhou yu cheng <caibiwanwan6@gmail.com>
2020-09-21 21:02:15 +0800

    9.21 完成了列表页的结构搭建

--- 160-project/src/router/index.js
+++ 160-project/src/router/index.js
@@ -8,6 +8,7 @@ let Mine = () => import('../views/Mine/Mine.vue');
 let Contact = () => import('../views/Contact/Contact.vue');
 let Reg = () => import('../views/Reg/Reg.vue');
 let Login = () => import('../views/Login/Login.vue');
+let Category = () => import('../views/List/children/Category.vue');
 Vue.use(VueRouter);
 
 // 路由表
@@ -26,6 +27,72 @@ const routes = [
         path: '/list',
         component: List,
         name: 'list',
+        children: [
+            {
+                path: 'pro',
+                component: Category,
+            },
+            {
+                path: 'family',
+                component: Category,
+            },
+            {
+                path: 'med',
+                component: Category,
+            },
+            {
+                path: 'care',
+                component: Category,
+            },
+            {
+                path: 'man',
+                component: Category,
+            },
+            {
+                path: 'mab',
+                component: Category,
+            },
+            {
+                path: 'interest',
+                component: Category,
+            },
+            {
+                path: 'cmed',
+                component: Category,
+            },
+            {
+                path: 'electrical',
+                component: Category,
+            },
+            {
+                path: 'digital',
+                component: Category,
+            },
+            {
+                path: 'beauty',
+                component: Category,
+            },
+            {
+                path: 'health',
+                component: Category,
+            },
+            {
+                path: 'cloth',
+                component: Category,
+            },
+            {
+                path: 'pet',
+                component: Category,
+            },
+            {
+                path: 'sproduct',
+                component: Category,
+            },
+            {
+                path: 'glasses',
+                component: Category,
+            },
+        ],
     },
     {
         path: '/cart',
---
+++ 160-project/src/views/List/children/Category.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>专科用药 {{ datalist }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            datalist: '阿莫西林',
+        };
+    },
+
+    components: {},
+
+    methods: {},
+
+    created() {
+        console.log(localStorage.getItem('type', 999));
+    },
+};
+</script>
+
+<style scoped></style>
