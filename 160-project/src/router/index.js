dbf2faa39d4a084ad055c368b59b0b8e509a1325
unknown <caibiwanwan6@gmail.com>
2020-09-22 22:13:02 +0800

    9.22 优化了购物车静态样式 进一步修改分类列表

--- 160-project/src/router/index.js
+++ 160-project/src/router/index.js
@@ -1,134 +1,67 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 // 路由懒加载
-let Home = () => import('../views/Home/Home.vue');
-let List = () => import('../views/List/List.vue');
-let Cart = () => import('../views/Cart/Cart.vue');
-let Mine = () => import('../views/Mine/Mine.vue');
-let Contact = () => import('../views/Contact/Contact.vue');
-let Reg = () => import('../views/Reg/Reg.vue');
-let Login = () => import('../views/Login/Login.vue');
-let Category = () => import('../views/List/children/Category.vue');
+let Home = () => import("../views/Home/Home.vue");
+let List = () => import("../views/List/List.vue");
+let Cart = () => import("../views/Cart/Cart.vue");
+let Mine = () => import("../views/Mine/Mine.vue");
+let Contact = () => import("../views/Contact/Contact.vue");
+let Reg = () => import("../views/Reg/Reg.vue");
+let Login = () => import("../views/Login/Login.vue");
 
 //import HelloWorld from '@/components/HelloWorld'
 const originalPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
-    return originalPush.call(this, location).catch(err => err);
+  return originalPush.call(this, location).catch((err) => err);
 };
 Vue.use(VueRouter);
 
 // 路由表
 const routes = [
-    {
-        path: '/home',
-        component: Home,
-        name: 'home',
-    },
-    // 重定向
-    {
-        path: '/',
-        redirect: '/home',
-    },
-    {
-        path: '/list',
-        component: List,
-        name: 'list',
-        children: [
-            {
-                path: 'pro',
-                component: Category,
-            },
-            {
-                path: 'family',
-                component: Category,
-            },
-            {
-                path: 'med',
-                component: Category,
-            },
-            {
-                path: 'care',
-                component: Category,
-            },
-            {
-                path: 'man',
-                component: Category,
-            },
-            {
-                path: 'mab',
-                component: Category,
-            },
-            {
-                path: 'interest',
-                component: Category,
-            },
-            {
-                path: 'cmed',
-                component: Category,
-            },
-            {
-                path: 'electrical',
-                component: Category,
-            },
-            {
-                path: 'digital',
-                component: Category,
-            },
-            {
-                path: 'beauty',
-                component: Category,
-            },
-            {
-                path: 'health',
-                component: Category,
-            },
-            {
-                path: 'cloth',
-                component: Category,
-            },
-            {
-                path: 'pet',
-                component: Category,
-            },
-            {
-                path: 'sproduct',
-                component: Category,
-            },
-            {
-                path: 'glasses',
-                component: Category,
-            },
-        ],
-    },
-    {
-        path: '/cart',
-        component: Cart,
-        name: 'cart',
-    },
-    {
-        path: '/mine',
-        component: Mine,
-        name: 'mine',
-    },
-    {
-        path: '/contact',
-        component: Contact,
-        name: 'contact',
-    },
-    {
-        path: '/reg',
-        component: Reg,
-        name: 'reg',
-    },
-    {
-        path: '/login',
-        component: Login,
-        name: 'login',
-    },
+  {
+    path: "/home",
+    component: Home,
+    name: "home",
+  },
+  // 重定向
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/list",
+    component: List,
+    name: "list",
+  },
+  {
+    path: "/cart",
+    component: Cart,
+    name: "cart",
+  },
+  {
+    path: "/mine",
+    component: Mine,
+    name: "mine",
+  },
+  {
+    path: "/contact",
+    component: Contact,
+    name: "contact",
+  },
+  {
+    path: "/reg",
+    component: Reg,
+    name: "reg",
+  },
+  {
+    path: "/login",
+    component: Login,
+    name: "login",
+  },
 ];
 // 注入实例
 const router = new VueRouter({
-    routes,
+  routes,
 });
 
 export default router;
--- 160-project/src/views/Cart/Cart.vue
+++ 160-project/src/views/Cart/Cart.vue
@@ -1,124 +1,137 @@
 <template>
+  <div>
+    <van-nav-bar
+      title="购物车"
+      left-text="返回"
+      right-text="需求清单"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
+    <van-notice-bar
+      :scrollable="false"
+      text="慧医卡，购买指定商品送健康豆，可抵现金使用！"
+    />
+    <!-- 商品信息 -->
+    <!-- vant空状态 -->
+    <van-empty
+      v-show="isShow"
+      description="购物车目前还没有商品"
+      image="https://img.yzcdn.cn/vant/custom-empty-image.png"
+    >
+      <van-button round type="danger" class="bottom-button">
+        去购物
+      </van-button>
+    </van-empty>
+
     <div>
-        <van-nav-bar
-            title="购物车"
-            left-text="返回"
-            right-text="需求清单"
-            left-arrow
-            @click-left="onClickLeft"
-            @click-right="onClickRight"
-        />
-        <van-notice-bar :scrollable="false" text="慧医卡，购买指定商品送健康豆，可抵现金使用！" />
-        <!-- 商品信息 -->
-        <!-- vant空状态 -->
-        <van-empty
-            v-show="isShow"
-            description="购物车目前还没有商品"
-            image="https://img.yzcdn.cn/vant/custom-empty-image.png"
-        >
-            <van-button round type="danger" class="bottom-button">
-                去购物
-            </van-button>
-        </van-empty>
+      <!-- vant复选框组 -->
+      <!-- vant布局 -->
+      <!-- vant滑动单元格 -->
+      <!-- vant商品卡片 -->
+      <!-- vant步进器 -->
+      <van-checkbox-group v-model="result">
+        <van-row style="background-color: white;">
+          <van-col span="2" style="margin-top: 35px;">
+            <van-checkbox name="item"></van-checkbox>
+          </van-col>
 
-        <div>
-            <!-- vant复选框组 -->
-            <!-- vant布局 -->
-            <!-- vant滑动单元格 -->
-            <!-- vant商品卡片 -->
-            <!-- vant步进器 -->
-            <van-checkbox-group v-model="result">
-                <van-row style="background-color: white;">
-                    <van-col span="2" style="margin-top: 35px;">
-                        <van-checkbox name="item"></van-checkbox>
-                    </van-col>
+          <van-col span="22">
+            <van-swipe-cell>
+              <van-card
+                price="14"
+                desc="商品描述"
+                title="商品名称"
+                thumb="item.data.mainPic"
+                class="goods-card"
+              >
+                <template #num>
+                  <van-stepper v-model="num" />
+                </template>
+              </van-card>
 
-                    <van-col span="22">
-                        <van-swipe-cell>
-                            <van-card
-                                price="14"
-                                desc="商品描述"
-                                title="商品名称"
-                                thumb="item.data.mainPic"
-                                class="goods-card"
-                            >
-                                <template #num>
-                                    <van-stepper v-model="num" />
-                                </template>
-                            </van-card>
-
-                            <template #right>
-                                <van-button
-                                    square
-                                    text="删除"
-                                    type="danger"
-                                    class="delete-button"
-                                />
-                            </template>
-                        </van-swipe-cell>
-                    </van-col>
-                </van-row>
-            </van-checkbox-group>
-        </div>
-
-        <!-- vant提交订单栏 -->
-        <van-submit-bar price="total" button-text="提交订单">
-            <van-checkbox v-model="checkedAll">全选</van-checkbox>
-        </van-submit-bar>
+              <template #right>
+                <van-button
+                  square
+                  text="删除"
+                  type="danger"
+                  class="delete-button"
+                />
+              </template>
+            </van-swipe-cell>
+          </van-col>
+        </van-row>
+      </van-checkbox-group>
     </div>
+
+    <!-- vant提交订单栏 -->
+    <van-submit-bar price="total" button-text="提交订单">
+      <van-checkbox v-model="checkedAll">全选</van-checkbox>
+    </van-submit-bar>
+  </div>
 </template>
 
 <script>
-import { Toast } from 'vant';
+import { Toast } from "vant";
 export default {
-    data() {
-        return {
-            carts: [], //添加到购物车的商品
-            result: [], //所有复选框选中的商品
-            checkedAll: false, //全选的状态
-            total: 0, //总价
-            isShow: true,
-            num: 0,
-        };
-    },
+  data() {
+    return {
+      carts: [], //添加到购物车的商品
+      result: [], //所有复选框选中的商品
+      checkedAll: false, //全选的状态
+      total: 0, //总价
+      isShow: true,
+      num: 0,
+    };
+  },
 
-    components: {},
+  components: {},
 
-    methods: {
-        onClickLeft() {
-            Toast('返回');
-        },
-        onClickRight() {
-            Toast('按钮');
-        },
+  methods: {
+    onClickLeft() {
+      Toast("返回");
     },
+    onClickRight() {
+      Toast("按钮");
+    },
+  },
 };
 </script>
 
 <style scoped>
 .van-nav-bar /deep/ .van-icon {
-    color: #00d6d5;
+  color: #00d6d5;
 }
 .van-nav-bar /deep/ .van-nav-bar__text {
-    color: #00d6d5;
+  color: #00d6d5;
 }
 .van-nav-bar /deep/ .van-nav-bar__title {
-    color: #00d6d5;
+  color: #00d6d5;
 }
 .van-notice-bar {
-    background-color: #fff;
-    padding: 0px 33px;
+  background-color: #fff;
+  padding: 0px 33px;
+}
+.van-checkbox {
+  margin-left: 0.6875rem;
 }
 .goods-card {
-    background-color: white;
+  background-color: white;
 }
-
+.van-submit-bar {
+  position: fixed;
+  bottom: 3.125rem;
+  left: 0;
+}
+.van-submit-bar__bar {
+  justify-content: space-between;
+}
 .delete-button {
-    height: 100%;
+  height: 100%;
 }
 
 .bottom-button {
-    width: 180px;
-    height: 40px;
+  width: 180px;
+  height: 40px;
 }
 </style>
