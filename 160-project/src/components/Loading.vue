78c95653a2fbb1a1e8bb4af3e4b37b1da8e6723f
StephenZhou <stephenzhou@StephenZhoudeMacBook-Pro.local>
2020-10-09 10:50:50 +0800

    10.9 封装vuex加载提示，优化页面效果

---
+++ 160-project/src/components/Loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="loading">
+    <van-loading size="24px" type="spinner" vertical>加载中...</van-loading>
+  </div>
+</template>
+
+<script>
+import { Loading } from "vant";
+export default {
+  name: "LOADING",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.loading {
+  position: fixed;
+  top: 35%;
+  left: 35%;
+  z-index: 121;
+  width: 30%;
+  height: 15%;
+  background: rgba(0, 0, 0, 0.3);
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  border-radius: 15%;
+}
+.van-loading {
+  margin-top: 20%;
+}
+</style>
--- 160-project/src/store/index.js
+++ 160-project/src/store/index.js
@@ -1,11 +1,23 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import user from './modules/user';
+import Vue from "vue";
+import Vuex from "vuex";
+import user from "./modules/user";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: {
-        user,
+  // 加载状态
+  state: {
+    LOADING: false,
+  },
+  mutations: {
+    showLoading(state) {
+      state.LOADING = true;
     },
+    hideLoading(state) {
+      state.LOADING = false;
+    },
+  },
+  modules: {
+    user,
+  },
 });
